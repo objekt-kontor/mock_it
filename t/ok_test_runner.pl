019eecf93e41b971144d5ed7cee1461ed2643c60
buchanan <craig.buchanan.is@gmail.com>
2014-02-19 15:29:01 +0100

    f5: readded one file test runner and removed ok test from eclipse include files

--- t/ok_test_runner.pl
+++ t/ok_test_runner.pl
@@ -1,16 +1,14 @@
 use FindBin;
 use lib "$FindBin::Bin";
 use lib "$FindBin::Bin/../lib";
-use lib "$FindBin::Bin/../../ok_test/lib";
 
-use Ok::Test::Runner 'load_tests';
-use Ok::Test::StdOutReporter;
+use Ok::Test;
 
-load_tests($FindBin::Bin);
+Ok::Test::Runner::load_tests($FindBin::Bin);
 
 use Test::More;
 plan tests => scalar(Ok::Test::get_loaded_tests) ;
 
-my $runner = new Ok::Test::Runner({listener => Ok::Test::StdOutReporter->new});
+my $runner = new Ok::Test::Runner({listener => Ok::Test::TAPReporter->new});
 
 $runner->run;
